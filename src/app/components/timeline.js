86d2255a15da7e4095ba676536fc5edfead4be08
Okan DAVUT <davut.okan@gmail.com>
2020-05-25 19:24:57 +0300

    Check if user name empty or not

--- src/app/components/timeline.js
+++ src/app/components/timeline.js
@@ -19,38 +19,25 @@ function TimelineComp(props) {
     const [repositories, setRepositories] = useState();
     const events = [];
     useEffect(() => {
-
+        if(props.username != ""){
         async function getUserRepositories(username) {
-            if (username != "") {
-                let response = await fetch(`https://api.github.com/users/${username}/repos?sort=created&direction=desc`,
-                {
-                    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                    mode: 'cors', // no-cors, *cors, same-origin
-                    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                    credentials: 'same-origin', // include, *same-origin, omit
-                    headers: {
-                      'Content-Type': 'application/json'
-                    },
-                    redirect: 'follow', // manual, *follow, error
-                    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-                  })
-                response = await response.json()
-                console.log(response.message);
-                if (response.message != "Not Found") {
-                    setRepositories(response)
-                }
-                else {
-                    console.log("No repo found!");
-                }
+            let response = await fetch(`https://api.github.com/users/${username}/repos?sort=created&direction=desc`)
+            response = await response.json()
+            if (response.message != "Not Found") {
+                setRepositories(response)
             }
-            setRepositories(undefined);
+            else {
+                console.log("No repo found!");
+            }
         }
         getUserRepositories(props.username);
+    }
+    else setRepositories(undefined);
     }, [props.username]);
 
     return (
         <>
-            <Timeline lineColor={repositories != undefined ? '#ddd' : 'white'}  >
+            <Timeline lineColor={repositories!= undefined ? '#ddd' : 'white'}  >
                 {repositories != undefined ? repositories.map((repo, i) => {
                     return (<TimelineItem
                         key={i}
