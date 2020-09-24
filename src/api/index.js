db5f4640f44278b590a4027a5e01c3c359a6dccc
Okan DAVUT <davut.okan@gmail.com>
2020-09-24 16:23:20 +0300

    Add per_page parameter Github api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 async function getUserRepositories(username) {
   let response = await fetch(
-    `https://api.github.com/users/${username}/repos?sort=created&direction=desc`
+    `https://api.github.com/users/${username}/repos?sort=created&direction=desc&per_page=300`
   );
   response = await response.json();
   return response;
