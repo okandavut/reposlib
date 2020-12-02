1e52b529f4a82cd045570476b8d9675c87ab6d83
Okan DAVUT <davut.okan@gmail.com>
2020-12-02 23:15:03 +0300

    Fix #10 - Load problem on user timeline page (#11)

--- src/pages/[user]/index.js
+++ src/pages/[user]/index.js
@@ -1,31 +1,27 @@
 import React, { useState, useEffect } from "react";
 import RepoTimeline from "../../components/timeline";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import getUserRepositories from "../../api/";
 
 function User() {
-    const router = useRouter()
-    const username = router.query.user;
-    const [repositories, setRepositories] = useState();
-    const [apiLimitError, setApiLimitError] = useState("");
+  const router = useRouter();
+  const username = router.query.user;
+  const [repositories, setRepositories] = useState();
+  const [apiLimitError, setApiLimitError] = useState("");
 
-    useEffect(() => {
-        async function getRepositories() {
-            let repositoriesResponse = await getUserRepositories(username);
-            !repositoriesResponse.message
-                ? setRepositories(repositoriesResponse)
-                : setApiLimitError(repositoriesResponse.message);
-        }
-        getRepositories();
+  useEffect(() => {
+    getUserRepositories(username).then((repositoriesResponse) => {
+      !repositoriesResponse.message
+        ? setRepositories(repositoriesResponse)
+        : setApiLimitError(repositoriesResponse.message);
+    });
+  }, [username]);
 
-    }, [username]);
-
-
-    return (
-        <>
-            <RepoTimeline repositories={repositories} apiLimitError={apiLimitError} />
-        </>
-    );
+  return (
+    <>
+      <RepoTimeline repositories={repositories} apiLimitError={apiLimitError} />
+    </>
+  );
 }
 
 export default User;
